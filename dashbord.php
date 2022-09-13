b6ff76ec3a71b40683960203d8999886e0586aa7
ElianLvX <villagran-85-85@hotmail.com>
2022-09-13 05:02:16 -0500

    actualizado

--- dashbord.php
+++ dashbord.php
@@ -94,27 +94,12 @@
   // Set data
   // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Setting_data
   series.data.setAll([{
-  category: "Lithuania",
+  category: "asd",
   value: 501.9
 }, {
-  category: "Czechia",
+  category: "cliente",
   value: 301.9
-}, {
-  category: "Ireland",
-  value: 201.1
-}, {
-  category: "Germany",
-  value: 165.8
-}, {
-  category: "Australia",
-  value: 139.9
-}, {
-  category: "Austria",
-  value: 128.3
-}, {
-  category: "UK",
-  value: 99
-}]);
+}]);gi
 
 series.appear(1000, 100);
 
